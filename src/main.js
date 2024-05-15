ed5e488799fb32f774c611cb72f24ee0942f9eb2
kartal5 <mainn2@hotmail.com>
2024-05-15 20:44:55 +0200

    register service worker in main.js

--- src/main.js
+++ src/main.js
@@ -5,3 +5,25 @@ import App from './App.vue'
 
 
 createApp(App).mount('#app')
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js').then((registration) => {
+        console.log('SW registered: ', registration);
+      }).catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
+    });
+  }
+
+// Register the service worker
+window.addEventListener('load', () => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js')
+        .then(registration => {
+          console.log('Service Worker registered: ', registration);
+        })
+        .catch(error => {
+          console.log('Service Worker registration failed: ', error);
+        });
+    }
+  });
